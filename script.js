8f14befe0b482127af43a360f5b5e0adc1f907c0
M. KALEEMULLAH KHAN QURESHI <136243025+kaleemzil@users.noreply.github.com>
2024-08-09 00:18:58 -0700

    add the code and push to origin

---
+++ script.js
@@ -0,0 +1,5 @@
+// Automatically refresh the page after the slider animation completes
+setTimeout(() => {
+    location.reload();
+  }, 16000); // Reload the page after 16 seconds (matching the animation duration)
+  
